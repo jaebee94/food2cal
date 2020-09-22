b6e8f776b5cacd9eacd17dd7a62a2fdcb42d4dd5
제유빈 <yubinje630@gmail.com>
2020-09-22 18:02:26 +0900

    [feat] Add post CRUD, comment CRUD | S03P23A411-13, S03P23A411-14

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
-    path('articles/', include('articles.urls'))
+    path('posts/', include('posts.urls'))
 ]
