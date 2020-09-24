15d75aaa8cfeaf8e9c6083dca81df863f93964f3
gaberani <khs0783@naver.com>
2020-09-25 01:21:20 +0900

    [feat] Add vuecookies, axios setting | S03P23A411-30

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueCookies from 'vue-cookies'
+import axios from 'axios'
 
+
+Vue.use(VueCookies)
+
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
 
 new Vue({
