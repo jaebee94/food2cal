db4d5406066eb16bff8b8bcad2af1408b69db05a
gaberani <khs0783@naver.com>
2020-09-25 01:27:27 +0900

    [feat] Add user join page | S03P23A411-32

--- frontend/src/views/users/UsersJoin.vue
+++ frontend/src/views/users/UsersJoin.vue
@@ -1,41 +1,58 @@
 <template>
   <v-container>
-    <v-row>
+    <v-row justify="center" align="center">
       <!-- 왼쪽 여백 -->
-      <v-col cols=4></v-col>
+      <v-col cols="2" md="3"></v-col>
       <!-- 회원가입 폼 -->
-      <v-col cols=4>
-        <v-form name="form" @submit.prevent style="width:318px">
-          <v-text-field
-            outlined
-            placeholder="아이디를 입력해주세요"
-            hide-details
-            name="id"
-            v-model="UserJoinData.id"
-          >
-          </v-text-field>
-          <v-text-field
-            @keyup.enter="login"
-            type="password"
-            outlined
-            placeholder="패스워드를 입력해주세요"
-            hide-details 
-            sytle="top:-1px;"
-            name="password"
-            v-model="UserJoinData.password1"
-          >
-          </v-text-field>
-          <v-text-field
-            @keyup.enter="login"
-            type="password"
-            outlined
-            placeholder="패스워드를 한 번 더 입력해주세요"
-            hide-details 
-            sytle="top:-1px;"
-            name="password"
-            v-model="UserJoinData.password2"
-          >
-          </v-text-field>
+      <v-col cols="8" md="6">
+        <v-form name="form" @submit.prevent>
+          <div class="input_field">
+            <v-text-field
+              outlined
+              placeholder="아이디를 입력해주세요"
+              hide-details
+              name="username"
+              v-model="UserJoinData.username"
+            >
+            </v-text-field>
+            <!-- ** 아이디 중복체크 버튼 ** -->
+            <v-btn
+              @click="CheckId"
+              class="checkid-btn white--text"
+              block 
+              depressed 
+              color="#FF4500"
+              height="48px"
+            >
+              중복확인
+            </v-btn>
+          </div>
+          <div class="input_field">
+            <v-text-field
+              @keyup.enter="login"
+              type="password"
+              outlined
+              placeholder="패스워드를 입력해주세요"
+              hide-details 
+              sytle="top:-1px;"
+              name="password"
+              v-model="UserJoinData.password1"
+            >
+            </v-text-field>
+          </div>
+          <div class="input_field">
+            <v-text-field
+              @keyup.enter="login"
+              type="password"
+              outlined
+              placeholder="패스워드를 한 번 더 입력해주세요"
+              hide-details 
+              sytle="top:-1px;"
+              name="password"
+              v-model="UserJoinData.password2"
+            >
+            </v-text-field>
+          </div>
           
           <!-- 아이디/비밀번호 찾기 -->
           <!-- <div class="login_append">
@@ -48,7 +65,7 @@
             <span>아이디/비밀번호 찾기</span> 
           </div> -->
           
-          <!-- 로그인 버튼 -->
+          <!-- 회원가입 버튼 -->
           <div @click="UserJoin(UserJoinData)" class="login_btn">
             <v-btn 
               block 
@@ -63,7 +80,7 @@
         </v-form>
       </v-col>
       <!-- 오른쪽 여백 -->
-      <v-col cols=4></v-col>
+      <v-col cols="2" md="3"></v-col>
     </v-row>
   </v-container>
 </template>
@@ -74,7 +91,7 @@ export default {
   data() {
     return {
       UserJoinData: {
-        id: null,
+        username: null,
         password1: null,
         password2: null,
         // weight: null,
@@ -83,11 +100,35 @@ export default {
     }
   },
   methods: {
+    // ** 아이디 중복체크 **
+    CheckId() {
+      // this.$http.post(process.env.VUE_SERVER_URL + '/rest-auth/signup/verify-email/')
+      this.$http.post('http://8a945e2757a9.ngrok.io' + '/rest-auth/signup/verify-email/')
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    },
+    // 회원가입
     UserJoin(UserJoinData) {
-      if (UserJoinData.id.trim()) {
-        console.log(UserJoinData)
+      if (UserJoinData.username.trim()) {
+        if (UserJoinData.password1.trim() === UserJoinData.password2.trim()) {
+          this.$http
+            .post(process.env.VUE_APP_SERVER_URL + 'rest-auth/signup/', UserJoinData)
+            .then(res => {
+              this.setCookie(res.data.key)
+              this.$router.push({ name: 'Home'})
+            })
+            // .catch(err => console.log(err))
+        } else {
+          alert('비밀번호가 일치하지 않습니다.')
+        }
+      } else {
+        alert('아이디가 비어있습니다.')
       }
-    }
+    },
+    setCookie(token){
+      this.$cookies.set('auth-token',token)
+      this.islogin = !this.islogin
+    },
     // join(joinData){
     //   axios.post(SERVER_URL + '/rest-auth/signup/', joinData)
     //   .then(res=>{
@@ -96,10 +137,15 @@ export default {
     //   })
     //   .catch(err=>console.log(err.response))
     // },
+  },
+  created() {
+    console.log(process.env)
   }
 }
 </script>
 
 <style>
-
+.input_field {
+  margin: 6px auto;
+}
 </style>
