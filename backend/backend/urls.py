4e816f62a5ed596c770801ca1a39343b3ce277f9
jaebee94 <woqls8449@gmail.com>
2020-09-24 19:32:40 +0900

    [feat] Add model, serializer | S03P23A411-37

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path('rest-auth/signup/', include('rest_auth.registration.urls')),
     path('posts/', include('posts.urls')),
     path('diets/', include('diets.urls')),
+    path('ai/', include('ai.urls'))
     # swagger
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
