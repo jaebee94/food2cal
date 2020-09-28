68eb2d6a5e2e78c19d8532c91ebdc5b4fb21d0a0
jaebee94 <woqls8449@gmail.com>
2020-09-28 23:42:17 +0900

    [feat] Add profile model
    Former-commit-id: 8be8a26cd8b756a6f7be88c0828db234d0c3b9a3

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 
 class User(AbstractUser):
     pass
 
+# Create your models here.
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     nickname = models.CharField(max_length=20)
+#     profile_image_path = models.CharField(max_length=255)
+#     gender = models.CharField(max_length=10)
+#     birth = models.DateField()
+#     height = models.IntegerField()
+#     weight = models.IntegerField()
+#     goal = models.IntegerField()
