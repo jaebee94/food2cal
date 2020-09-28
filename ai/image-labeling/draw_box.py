fad519c2cdd0d2e8882593ed1a002275d11bdcd0
gaberani <khs0783@naver.com>
2020-09-28 11:55:19 +0900

    [feat] Add beat image check, RGB check | S03P23A411-17

--- ai/image-labeling/draw_box.py
+++ ai/image-labeling/draw_box.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 from matplotlib.widgets import RectangleSelector
 from generate_xml import write_xml
+from PIL import Image
 
 # global constants
 img = None
@@ -13,7 +14,19 @@ object_list = []
 # constants
 image_folder = 'images'
 savedir = 'annotations'
-obj = 'fidget_spinner'
+
+'''
+밥   : 밥(잡곡밥), 김밥
+김치 : 배추김치
+찌개 : 김치찌개
+면   : 라면
+고기 : 불고기
+전   : 계란말이
+기타 : 피자, 후라이드치킨, 양념치킨
+'''
+
+# ** 해당 음식 이름 하나로 바꿔서 실행) **
+obj = 'fidget_spinner'  # 바꿔야할 곳
 
 def line_select_callback(clk, rls):
     global tl_list
@@ -47,17 +60,24 @@ for n, image_file in enumerate(os.scandir(image_folder)):
     mngr = plt.get_current_fig_manager()
     mngr.window.setGeometry(250, 40, 800, 600)
     image = cv2.imread(image_file.path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    ax.imshow(image)
+    # print(image_file.path)
+    if 'Img' in image_file.path and image_file.path.endswith("jpg"):
+        im = Image.open(image_file.path)
+        mode_to_bpp = {"1": 1, "L": 8, "P": 8, "RGB": 24, "RGBA": 32, "CMYK": 32, "YCbCr": 24, "LAB": 24, "HSV": 24, "I": 32, "F": 32}
+        bpp = mode_to_bpp[im.mode]
+        # 이미지 색상 모드 & 비트 수준 체크
+        if im.mode == "RGB" and bpp == 24:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            ax.imshow(image)
 
-    toggle_selector.RS = RectangleSelector(
-        ax, line_select_callback,
-        drawtype='box', useblit=True,
-        # button=[1]: it means left mouse click
-        button=[1], minspanx=5, minspany=5,
-        spancoords='pixels', interactive=True
-    )
-    bbox = plt.connect('button_press_event', toggle_selector)
-    key = plt.connet('key_press_event', onkeypress)
-    plt.show()
-    plt.close(fig)
+            toggle_selector.RS = RectangleSelector(
+                ax, line_select_callback,
+                drawtype='box', useblit=True,
+                # button=[1]: it means left mouse click
+                button=[1], minspanx=5, minspany=5,
+                spancoords='pixels', interactive=True
+            )
+            bbox = plt.connect('button_press_event', toggle_selector)
+            key = plt.connect('key_press_event', onkeypress)
+            plt.show()
+            plt.close(fig)
