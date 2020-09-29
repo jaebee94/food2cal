640318d181b261277ae165a8f44d5feeb40ac0af
gaberani <khs0783@naver.com>
2020-09-29 19:24:57 +0900

    [feat] Add mypage.vue | S03P23A411-60
    Former-commit-id: 05e9837d7e5d8cfc3f1aec362a760b23daaad86a

---
+++ frontend/src/views/users/Mypage.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container>
+     <v-card
+    class="mx-auto text-center"
+    color="green"
+    dark
+    max-width="600"
+  >
+    <v-card-text>
+      <v-sheet color="rgba(0, 0, 0, .12)">
+        <v-sparkline
+          :value="value"
+          color="rgba(255, 255, 255, .7)"
+          height="100"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+          <template v-slot:label="item">
+            ${{ item.value }}
+          </template>
+        </v-sparkline>
+      </v-sheet>
+    </v-card-text>
+
+    <v-card-text>
+      <div class="display-1 font-weight-thin">
+        Sales Last 24h
+      </div>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-actions class="justify-center">
+      <v-btn
+        block
+        text
+      >
+        Go to Report
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Mypage',
+    data() {
+        return {
+            value: [
+                423,
+                446,
+                675,
+                510,
+                590,
+                610,
+                760,
+            ],
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
