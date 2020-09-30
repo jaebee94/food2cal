30da4768991640e462587ecad84740b40ad5a1f1
제유빈 <yubinje630@gmail.com>
2020-09-30 22:50:50 +0900

    [feat] Modify diet_create (Add new food to an existing diet), Add food_delete | S03P23A411-68, S03P23A411-69
    Former-commit-id: eec9109abc97c15db9a548a44796dcba514c8d30

--- backend/diets/urls.py
+++ backend/diets/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'diets'
 
 urlpatterns = [
+    path('', views.diet_create, name='diet_create'),
     path('<year_month>/', views.diet_calendar, name='diet_calendar'),
     path('statistics/', views.diet_statistics, name='diet_statistics'),
+    path('<int:diet_id>/foods/<int:food_id>/', views.food_delete, name='food_delete'),
 ]
 
