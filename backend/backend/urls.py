ecd844ede0cd396ef236eaebe4bab24aab37c2ae
jaebee94 <woqls8449@gmail.com>
2020-10-04 16:22:18 +0900

    [fix] Modify urls.py
    Former-commit-id: 498e6face79c94abd14b8dab0943eca538d6e060

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,9 +25,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/signup/', include('rest_auth.registration.urls')),
+    path('users/', include('rest_auth.urls')),
+    path('users/signup/', include('rest_auth.registration.urls')),
     path('users/', include('users.urls')),
 
     path('posts/', include('posts.urls')),
