2f18f687a3f546b04059048a4486d01da5d2ea17
gaberani <khs0783@naver.com>
2020-10-04 21:46:32 +0900

    [feat] Add all userjoindata form stepper | S03P23A411-32
    Former-commit-id: b578aca20f9820c8be0560111bd4144d9f727496

--- frontend/src/views/users/UsersJoin.vue
+++ frontend/src/views/users/UsersJoin.vue
@@ -3,6 +3,7 @@
     <!-- 스테퍼 헤더 상태 바 -->
     <v-stepper-header>
       <v-stepper-step step="1"
+        color="#F84A0D"
         :complete="e1 > 1"
       >
         목표
@@ -11,6 +12,7 @@
       <v-divider></v-divider>
 
       <v-stepper-step step="2"
+        color="#F84A0D"
         :complete="e1 > 2"
       >
         몸무게
@@ -19,6 +21,7 @@
       <v-divider></v-divider>
 
       <v-stepper-step step="3"
+        color="#F84A0D"
         :complete="e1 > 3"
       >
         성별
@@ -27,6 +30,7 @@
       <v-divider></v-divider>
 
       <v-stepper-step step="4"
+        color="#F84A0D"
         :complete="e1 > 4"
       >
         생년월일
@@ -34,7 +38,9 @@
 
       <v-divider></v-divider>
 
-      <v-stepper-step step="5">
+      <v-stepper-step step="5"
+        color="#F84A0D"
+      >
         회원 정보 입력
       </v-stepper-step>
     </v-stepper-header>
@@ -42,12 +48,94 @@
     <!-- 스테퍼 내용창 -->
     <v-stepper-items>
       <v-stepper-content step="1">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
-        <!-- 버튼 -->
+        <v-row 
+          justify="center" 
+          class="mb-4"
+        >
+          <h3>당신의 목표는 무엇인가요?</h3>
+        </v-row>
+        <!-- 체중 감소 버튼 -->
+        <v-row
+          justify="center" 
+          class="mb-4"
+        >
+          <v-btn
+            v-if="this.UserJoinData.goal === '체중 감소'"
+            @click="onClickGoal(4)"
+            large
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 감소
+          </v-btn>
+          <v-btn
+            v-else
+            @click="onClickGoal(1)"
+            large
+            outlined
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 감소
+          </v-btn>
+        </v-row>
+        <!-- 체중 유지 버튼 -->
+        <v-row
+          justify="center"
+          class="mb-4"
+        >
+          <v-btn
+            v-if="this.UserJoinData.goal === '체중 유지'"
+            @click="onClickGoal(4)"
+            large
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 유지
+          </v-btn>
+          <v-btn
+            v-else
+            @click="onClickGoal(2)"
+            large
+            outlined
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 유지
+          </v-btn>
+        </v-row>
+        <!-- 체중 증가 버튼 -->
+        <v-row
+          justify="center"
+          class="mb-4"  
+        >
+          <v-btn
+            v-if="this.UserJoinData.goal === '체중 증가'"
+            @click="onClickGoal(4)"
+            large
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 증가
+          </v-btn>
+          <v-btn
+            v-else
+            @click="onClickGoal(3)"
+            large
+            outlined
+            color="#FF890E"
+            dark
+            elevation="3"
+          >
+            체중 증가
+          </v-btn>
+        </v-row>
+        <!-- 스테퍼 버튼 -->
         <v-btn
           color="primary"
           @click="e1 = 2"
@@ -62,12 +150,31 @@
       </v-stepper-content>
 
       <v-stepper-content step="2">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
-        <!-- 버튼 -->
+        <v-row
+          justify="center"
+          class="mb-4"
+        >
+          <h3>당신의 키와 몸무게는?</h3>
+        </v-row>
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-form>
+            <v-text-field
+              v-model="this.UserJoinData.height"
+              label="키(cm)"
+              type="number"
+              color="#FF890E"
+            ></v-text-field>
+            <v-text-field
+              v-model="this.UserJoinData.weight"
+              label="체중(kg)"
+              type="number"
+              color="#FF890E"
+            ></v-text-field>
+          </v-form>
+        </v-row>
         <v-btn
           color="primary"
           @click="e1 = 3"
@@ -82,13 +189,43 @@
       </v-stepper-content>
 
       <v-stepper-content step="3">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
-
-        <!-- 버튼 -->
+        <v-row justify="center">
+          <h3>당신의 성별은 무엇인가요?</h3>
+        </v-row>
+        <v-row 
+          justify="center"
+          class="mt-5 mb-10"
+        >
+          <v-col cols=1></v-col>
+          <v-col cols=1>
+            <v-icon
+              v-if="this.UserJoinData.gender==='femail'"
+              @click="onClickWoman"
+              x-large
+              color="#FF890E"
+            >mdi-face-woman</v-icon>
+            <v-icon 
+              v-else
+              @click="onClickWoman"
+              x-large
+            >mdi-face-woman</v-icon>
+          </v-col>
+          <v-col cols=1 align-self="auto"><div></div></v-col>
+          <v-col cols=2>
+            <v-icon
+              v-if="this.UserJoinData.gender==='mail'"
+              @click="onClickMan"
+              x-large
+              color="#FF890E"
+            >mdi-face</v-icon>
+            <v-icon 
+              v-else
+              @click="onClickMan"  
+              x-large
+            >mdi-face</v-icon>
+          </v-col>
+        </v-row>
+        
         <v-btn
           color="primary"
           @click="e1 = 4"
@@ -101,19 +238,20 @@
           이전
         </v-btn>
       </v-stepper-content>
+
       <v-stepper-content step="4">
-        <v-row justify="center">
-          <v-col cols=12>
-            <h3>당신의 생일은 언제인가요?</h3>
-          </v-col>
-          <v-col cols=12>
-            <v-date-picker 
+        <v-row
+          justify="center">
+          <h3>당신의 생일은 언제인가요?</h3>
+        </v-row>
+        <v-row 
+          class="mt-4"
+          justify="center">
+          <v-date-picker 
             v-model="UserJoinData.birth"
             locale="ko-kr"
           ></v-date-picker>
-          </v-col>
         </v-row>
-        <!-- 버튼 -->
         <v-btn
           color="primary"
           @click="e1 = 5"
@@ -221,8 +359,6 @@
         </v-btn>
       </v-stepper-content>
     </v-stepper-items>
-
-    
   </v-stepper>
 </template>
 
@@ -233,16 +369,15 @@ export default {
     return {
       e1: 1,
       UserJoinData: {
+        goal: null,
+        weight: null,
+        height: null,
+        birth: new Date().toISOString().substr(0, 10),
+        gender: null,
         username: null,
         email: null,
         password1: null,
         password2: null,
-        // weight: null,
-        // goal: null,
-        // height: null,
-        birth: new Date().toISOString().substr(0, 10),
-        // gender: null,
-        // preference: null,
       },
       // emailflag: false
     }
@@ -277,10 +412,44 @@ export default {
         alert('닉네임이 비어있습니다.')
       }
     },
+    // 회원가입과 동시에 인증키 쿠키 셋팅
     setCookie(token){
       this.$cookies.set('auth-token',token)
       this.islogin = !this.islogin
     },
+
+    // 성별 입력 메서드
+    onClickWoman() {
+      console.log('Click Woman Icon')
+      if (this.UserJoinData.gender !== 'femail') {
+        this.UserJoinData.gender='femail'
+      }
+      else {
+        this.UserJoinData.gender = null
+      } 
+    },
+    onClickMan() {
+      console.log('Click Man Icon')
+      if (this.UserJoinData.gender !== 'mail') {
+        this.UserJoinData.gender='mail'
+      }
+      else {
+        this.UserJoinData.gender = null
+      }
+    },
+
+    // 목표 입력 메서드
+    onClickGoal(num) {
+      if (num === 1) {
+        this.UserJoinData.goal = "체중 감소"
+      } else if (num === 2) {
+        this.UserJoinData.goal = "체중 유지"
+      } else if (num === 3) {
+        this.UserJoinData.goal = "체중 증가"
+      } else if (num === 4) {
+        this.UserJoinData.goal = null
+      }
+    }
   },
 }
 </script>
