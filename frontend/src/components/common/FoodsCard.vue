935a934f65c2bfa2afce5f4426bb3fe0c8519fb2
kimjanghu <kjh921030@gmail.com>
2020-10-05 02:26:02 +0900

    [feat] Get monthly diets | S03P23A411-87
    Former-commit-id: d16223653bf58da1e424c85ec9e6ed2b864d272d

--- frontend/src/components/common/FoodsCard.vue
+++ frontend/src/components/common/FoodsCard.vue
@@ -8,6 +8,10 @@
         <v-icon>{{ category.icon }}</v-icon>
         <p class="ml-3 mb-0">{{ category.time }}</p>
       </div>
+      <v-spacer></v-spacer>
+      <v-icon @click="goToDietsCreate">
+        mdi-plus-circle-outline
+      </v-icon>
     </v-card-title>
 
     <v-card-actions>
@@ -34,8 +38,17 @@
       <div v-show="show">
         <v-divider></v-divider>
 
-        <v-card-text>
-          food
+        <v-card-text v-if="foodInfo && foodInfo[category.type].length">
+          <div v-for="food in foodInfo[category.type]" :key="food.id">
+            {{ food }}
+            <!-- <v-spacer></v-spacer> -->
+            <v-icon>
+              mdi-close-circle-outline
+            </v-icon>
+          </div>
+        </v-card-text>
+        <v-card-text v-else>
+          음식 정보가 없습니다.
         </v-card-text>
       </div>
     </v-expand-transition>
@@ -43,14 +56,74 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      show: false,
-    }),
-    props: {
-      category: {
-        type: Object
-      }
+import SERVER from '@/libs/api'
+import constants from '@/libs/constants'
+
+export default {
+  data () {
+    return {
+      show: false
     }
+  },
+  props: {
+    category: {
+      type: Object
+    },
+    foodInfo: {
+      type: Object
+    },
+    date: {
+      type: String
+    }
+  },
+  created () {
+  },
+  methods: {
+    goToDietsCreate() {
+      this.$router.push({ name: constants.URL_TYPE.UPLOAD.DIET, query: { date: this.date, type: this.category.type} })
+    },
+    addDiet() {
+      const config = {
+        headers: {
+          Authorization: `Token ${this.$cookies.get(`auth-token`)}`
+        }
+      }
+
+      const today = this.date
+
+      const dietData = {
+        diet: {
+          date: today,
+          category: this.category.type,
+        },
+        food: [
+          {
+            food_name: this.foodInfo.food_name,
+            amount: this.foodInfo.ammount,
+            calorie: this.foodInfo.calorie,
+            carbohydrate: this.foodInfo.carbohydrate,
+            protein: this.foodInfo.protein,
+            fat: this.foodInfo.fat
+          }
+        ]
+      }
+      this.$http
+        .post(process.env.VUE_APP_SERVER_URL + SERVER.ROUTES.createDiet, dietData, config)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => console.log(err.response.data))
+    },
+    // deleteDiet() {
+    //   const config = {
+    //     headers: {
+    //       Authorization: `Token ${this.$cookies.get(`auth-token`)}`
+    //     }
+    //   }
+
+    //   this.$http
+    //     .delete(process.env.VUE_APP_SERVER_URL + SERVER.ROUTES.createDiet + 'foods/' + `${}/`)
+    // }
   }
+}
 </script>
--- frontend/src/views/calendar/Diary.vue
+++ frontend/src/views/calendar/Diary.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <Calendar />
-    <FoodsCard :category="{ time: '아침', icon: 'mdi-weather-sunset-up' }" />
-    <FoodsCard :category="{ time: '점심', icon: 'mdi-white-balance-sunny' }" />
-    <FoodsCard :category="{ time: '저녁', icon: 'mdi-weather-sunset' }" />
-    <FoodsCard :category="{ time: '간식/기타', icon: 'mdi-theme-light-dark' }" />
+    <Calendar @select-date="updateFoodInfo" @date="updateDate" />
+    <div v-if="foodInfo">
+      {{ foodInfo.calorie }}
+      {{ foodInfo.carbohydrate }}
+      {{ foodInfo.protein }}
+      {{ foodInfo.fat }}
+    </div>
+    
+    <FoodsCard :category="{ time: '아침', icon: 'mdi-weather-sunset-up', type: 'MO' }" :foodInfo="foodInfo" :date="date" />
+    <FoodsCard :category="{ time: '점심', icon: 'mdi-white-balance-sunny', type: 'LU' }" :foodInfo="foodInfo" :date="date" />
+    <FoodsCard :category="{ time: '저녁', icon: 'mdi-weather-sunset', type: 'DI' }" :foodInfo="foodInfo" :date="date" />
+    <FoodsCard :category="{ time: '간식/기타', icon: 'mdi-theme-light-dark', type: 'SN' }" :foodInfo="foodInfo" :date="date" />
   </div>
 </template>
 
@@ -14,9 +21,25 @@ import Calendar from '@/components/common/Calendar'
 
 export default {
   name: 'Diary',
+  data () {
+    return {
+      foodInfo: null,
+      date: null
+    }
+  },
   components: {
     FoodsCard,
     Calendar
+  },
+  methods: {
+    updateFoodInfo(foodInfo) {
+      this.foodInfo = foodInfo
+      console.log(this.foodInfo)
+    },
+    updateDate(date) {
+      this.date = date
+      console.log(this.date)
+    }
   }
 }
 </script>
