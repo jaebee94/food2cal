481177032ded17672cdf20888f81cf1b1bc07835
jaebee94 <woqls8449@gmail.com>
2020-10-04 23:56:04 +0900

    [fix] Fix bug
    Former-commit-id: 2f76ad487b1829bfd5927389013f13ee7656b4d1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://j3a411.p.ssafy.io']
+ALLOWED_HOSTS = ['j3a411.p.ssafy.io']
 # ALLOWED_HOSTS = ['*']
 
 
