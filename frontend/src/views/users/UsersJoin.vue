106af368d1621735019bddbd0d2d545e8a11c930
gaberani <khs0783@naver.com>
2020-10-04 13:57:14 +0900

    [feat] Add date picker on signup form | S03P23A411-32
    Former-commit-id: 6e6b2bdc40035873494ce61c06be37bb7de7b664

--- frontend/src/views/users/UsersJoin.vue
+++ frontend/src/views/users/UsersJoin.vue
@@ -1,31 +1,45 @@
 <template>
   <v-stepper v-model="e1">
-    <!-- 스테퍼 헤더 부분 -->
+    <!-- 스테퍼 헤더 상태 바 -->
     <v-stepper-header>
-      <v-stepper-step
+      <v-stepper-step step="1"
         :complete="e1 > 1"
-        step="1"
       >
-        키
+        목표
       </v-stepper-step>
 
       <v-divider></v-divider>
 
-      <v-stepper-step
+      <v-stepper-step step="2"
         :complete="e1 > 2"
-        step="2"
       >
         몸무게
       </v-stepper-step>
 
       <v-divider></v-divider>
 
-      <v-stepper-step step="3">
+      <v-stepper-step step="3"
+        :complete="e1 > 3"
+      >
+        성별
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="4"
+        :complete="e1 > 4"
+      >
+        생년월일
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="5">
         회원 정보 입력
       </v-stepper-step>
     </v-stepper-header>
 
-    <!-- 스테퍼 내용 부분 -->
+    <!-- 스테퍼 내용창 -->
     <v-stepper-items>
       <v-stepper-content step="1">
         <v-card
@@ -33,7 +47,6 @@
           color="grey lighten-1"
           height="200px"
         ></v-card>
-
         <!-- 버튼 -->
         <v-btn
           color="primary"
@@ -54,14 +67,13 @@
           color="grey lighten-1"
           height="200px"
         ></v-card>
-
+        <!-- 버튼 -->
         <v-btn
           color="primary"
           @click="e1 = 3"
         >
           다음
         </v-btn>
-
         <v-btn text
           @click="e1 = 1"
         >
@@ -70,106 +82,147 @@
       </v-stepper-content>
 
       <v-stepper-content step="3">
-          <v-container>
-            <v-row justify="center" align="center">
-              <!-- 왼쪽 여백 -->
-              <v-col cols="2" md="3"></v-col>
-              <!-- 회원가입 폼 -->
-              <v-col cols="8" md="6">
-                <v-form name="form" @submit.prevent>
-                  <div class="input_field">
-                    <v-text-field
-                      outlined
-                      placeholder="닉네임을 입력해주세요"
-                      hide-details
-                      name="email"
-                      v-model="UserJoinData.email"
-                    >
-                    </v-text-field>
-                  </div>
-                  <div class="input_field">
-                    <v-text-field
-                      outlined
-                      placeholder="이메일을 입력해주세요"
-                      hide-details
-                      name="username"
-                      v-model="UserJoinData.username"
-                    >
-                    </v-text-field>
-                    <!-- ** 이메일 중복체크 버튼 ** -->
-                    <!-- <v-btn
-                      v-if="!emailflag"
-                      @click="CheckEmail"
-                      class="checkemail-btn white--text"
-                      block 
-                      depressed 
-                      color="#FF4500"
-                      height="48px"
-                    >
-                      이메일 중복확인
-                    </v-btn> -->
-                  </div>
-                  <div class="input_field">
-                    <v-text-field
-                      @keyup.enter="login"
-                      type="password"
-                      outlined
-                      placeholder="패스워드를 입력해주세요"
-                      hide-details 
-                      sytle="top:-1px;"
-                      name="password"
-                      v-model="UserJoinData.password1"
-                    >
-                    </v-text-field>
-                  </div>
-                  <div class="input_field">
-                    <v-text-field
-                      @keyup.enter="login"
-                      type="password"
-                      outlined
-                      placeholder="패스워드를 한 번 더 입력해주세요"
-                      hide-details 
-                      sytle="top:-1px;"
-                      name="password"
-                      v-model="UserJoinData.password2"
-                    >
-                    </v-text-field>
-                  </div>
-                  
-                  <!-- 회원가입 버튼 -->
-                  <div @click="UserJoin(UserJoinData)" class="login_btn">
-                    <v-btn 
-                      block 
-                      depressed 
-                      color="#FF4500"
-                      height="48px"
-                      class="white--text"
-                      rounded
-                    >
-                      Join to F2C
-                    </v-btn>
-                  </div>
-                </v-form>
-              </v-col>
-              <!-- 오른쪽 여백 -->
-              <v-col cols="2" md="3"></v-col>
-            </v-row>
-          </v-container>
+        <v-card
+          class="mb-12"
+          color="grey lighten-1"
+          height="200px"
+        ></v-card>
 
         <!-- 버튼 -->
-        <!-- <v-btn
+        <v-btn
           color="primary"
-          @click="e1 = 1"
+          @click="e1 = 4"
         >
-          Continue
-        </v-btn> -->
+          다음
+        </v-btn>
         <v-btn text
           @click="e1 = 2"
         >
           이전
         </v-btn>
       </v-stepper-content>
+      <v-stepper-content step="4">
+        <v-row justify="center">
+          <v-col cols=12>
+            <h3>당신의 생일은 언제인가요?</h3>
+          </v-col>
+          <v-col cols=12>
+            <v-date-picker 
+            v-model="UserJoinData.birth"
+            locale="ko-kr"
+          ></v-date-picker>
+          </v-col>
+        </v-row>
+        <!-- 버튼 -->
+        <v-btn
+          color="primary"
+          @click="e1 = 5"
+        >
+          다음
+        </v-btn>
+        <v-btn text
+          @click="e1 = 3"
+        >
+          이전
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="5">
+        <v-container>
+          <v-row justify="center" align="center">
+            <!-- 왼쪽 여백 -->
+            <v-col cols="2" md="3"></v-col>
+            <!-- 회원가입 폼 -->
+            <v-col cols="8" md="6">
+              <v-form name="form" @submit.prevent>
+                <div class="input_field">
+                  <v-text-field
+                    outlined
+                    placeholder="닉네임을 입력해주세요"
+                    hide-details
+                    name="username"
+                    v-model="UserJoinData.username"
+                  >
+                  </v-text-field>
+                </div>
+                <div class="input_field">
+                  <v-text-field
+                    outlined
+                    placeholder="이메일을 입력해주세요"
+                    hide-details
+                    name="email"
+                    v-model="UserJoinData.email"
+                  >
+                  </v-text-field>
+                  <!-- ** 이메일 중복체크 버튼 ** -->
+                  <!-- <v-btn
+                    v-if="!emailflag"
+                    @click="CheckEmail"
+                    class="checkemail-btn white--text"
+                    block 
+                    depressed 
+                    color="#FF4500"
+                    height="48px"
+                  >
+                    이메일 중복확인
+                  </v-btn> -->
+                </div>
+                <div class="input_field">
+                  <v-text-field
+                    @keyup.enter="login"
+                    type="password"
+                    outlined
+                    placeholder="패스워드를 입력해주세요"
+                    hide-details 
+                    sytle="top:-1px;"
+                    name="password"
+                    v-model="UserJoinData.password1"
+                  >
+                  </v-text-field>
+                </div>
+                <div class="input_field">
+                  <v-text-field
+                    @keyup.enter="login"
+                    type="password"
+                    outlined
+                    placeholder="패스워드를 한 번 더 입력해주세요"
+                    hide-details 
+                    sytle="top:-1px;"
+                    name="password"
+                    v-model="UserJoinData.password2"
+                  >
+                  </v-text-field>
+                </div>
+                
+                <!-- 회원가입 버튼 -->
+                <div @click="UserJoin(UserJoinData)" class="login_btn">
+                  <v-btn 
+                    block 
+                    depressed 
+                    color="#FF4500"
+                    height="48px"
+                    class="white--text"
+                    rounded
+                  >
+                    Join to F2C
+                  </v-btn>
+                </div>
+              </v-form>
+            </v-col>
+            <!-- 오른쪽 여백 -->
+            <v-col cols="2" md="3"></v-col>
+          </v-row>
+        </v-container>
+        <!-- 버튼 -->
+        <v-btn text
+          @click="e1 = 4"
+        >
+          이전
+        </v-btn>
+      </v-stepper-content>
     </v-stepper-items>
+
+    
   </v-stepper>
 </template>
 
@@ -185,11 +238,11 @@ export default {
         password1: null,
         password2: null,
         // weight: null,
-        // goalweight: null,
+        // goal: null,
         // height: null,
-        // age: null,
-        // preference: null,
+        birth: new Date().toISOString().substr(0, 10),
         // gender: null,
+        // preference: null,
       },
       // emailflag: false
     }
@@ -211,7 +264,7 @@ export default {
       if (UserJoinData.username.trim()) {
         if (UserJoinData.password1 === UserJoinData.password2) {
           this.$http
-            .post(process.env.VUE_APP_SERVER_URL + 'rest-auth/signup/', UserJoinData)
+            .post(process.env.VUE_APP_SERVER_URL + '/rest-auth/signup/', UserJoinData)
             .then(() => {
               // this.setCookie(res.data.key)
               this.$router.push({ name: 'Home'})
