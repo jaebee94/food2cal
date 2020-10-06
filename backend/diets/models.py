5a072025faf1e6070b43f8b08a033ff8a942f6f1
제유빈 <yubinje630@gmail.com>
2020-10-06 20:48:15 +0900

    [feat] add standards in user profile, diet, Add more data at ai_nutrition | S03P23A411-95 , S03P23A411-96, S03P23A411-97
    Former-commit-id: 971afce83127f223770c2506dab130ce58faedb1

--- backend/diets/models.py
+++ backend/diets/models.py
@@ -4,6 +4,7 @@ from posts.models import Post
 
 class Diet(models.Model):
     created_at = models.CharField(max_length=100)
+    standard = models.IntegerField(null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = (
         ('MO', '아침'), ('LU', '점심'), ('DI', '저녁'), ('SN', '간식기타')
