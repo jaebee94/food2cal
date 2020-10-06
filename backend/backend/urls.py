b0f677a61e5390cfd72f070ab4774b363d176d64
jaebee94 <woqls8449@gmail.com>
2020-10-07 01:01:49 +0900

    [fix] Fix import
    Former-commit-id: 961bc8fd94eb7d5ecca984754a58f5749f49ea85

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+# from django.conf.urls import url
 from rest_framework import permissions
 
 from django.conf.urls.static import static
