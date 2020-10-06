f2c4a1ca3bde417c6b05aeef70dead3dbb7d82e2
jaebee94 <woqls8449@gmail.com>
2020-10-07 00:54:17 +0900

    [fix] Fix bug
    Former-commit-id: 8793db41bd40dd7722ac8a44c62371202f66f1f0

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path('diets/', include('diets.urls')),
     path('predict/', include('ai.urls')),
     # swagger
-    path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
+    # path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
+    # path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
