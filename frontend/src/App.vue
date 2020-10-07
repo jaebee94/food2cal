e8f6563a63908d7cb75da4869462bd17b134aaf8
gaberani <khs0783@naver.com>
2020-10-07 14:22:00 +0900

    [style] Delete  rerendor props emit event
    Former-commit-id: 262fdba2a2e9e262d0a632c036df074cefbc27b8

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <v-app>
     <AppBar
-      v-if="rerenderflag"
-      @submit-logout="forceRerender"
     />
     <v-main class="mb-16">
       <router-view 
         :key="$route.fullPath"
-        @submit-login="forceRerender"
       ></router-view>
     </v-main>
     <BottomNavbar
-      v-if="rerenderflag"
-      @submit-logout="forceRerender"
     />
   </v-app>
 </template>
@@ -28,17 +23,9 @@ export default {
     AppBar,
     BottomNavbar
   },
-
   data: () => ({
-    rerenderflag:true,
   }),
   methods: {
-    forceRerender() {
-        this.rerenderflag = false;
-        this.$nextTick(() => {
-          this.rerenderflag = true;
-        })
-      },
   }
 };
 </script>
